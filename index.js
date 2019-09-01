7fea62a1fd82ba9f7ed9c8504d3bbafd6680d906
Matt <matthew.endicott46@gmail.com>
2019-09-01 08:06:12 -0600

    inital commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var THREE = require('three');
+
+// - One ( scene creation )
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+
+// -Second (object creation)
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+// - Third (animation)
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+animate();
